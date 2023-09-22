9e365b0c97e234a94e83f6e5a5287a27e3f8e284
Mesbah Ayoub <ayoubmesbah20@gmail.com>
2023-09-22 12:06:18 +0100

    ajouter au code une instriction

---
+++ test code.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"Bonjour c'est Ayoub Mesbah "
+
+a = input ('Saisir votre valeur :')
+a = float(a)
+b=3
+d=a+b
+if d>3 :
+    print('a est superieur 0')
+else :
+    print('d est egale 0 ')      
+
